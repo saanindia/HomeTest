fd0ba4ae3a2b8b517c755fcc3571fcaaca2f5657
saanindia <saan.india@gmail.com>
2015-09-27 18:40:16 +0530

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -5,9 +5,9 @@ ls
 echo "Transfered successfully"
 touch yes_test.sh
 
-bundle install
+#bundle install
 
-bundle exec veewee vbox define 'ubuntu-14' 'ubuntu-14.04.3-server-amd64'
-bundle exec veewee vbox build 'ubuntu-14' --force --auto --nogui
-bundle exec veewee vbox validate 'ubuntu-14'
-bundle exec vagrant basebox export 'ubuntu-14' --force
+#bundle exec veewee vbox define 'ubuntu-14' 'ubuntu-14.04.3-server-amd64'
+#bundle exec veewee vbox build 'ubuntu-14' --force --auto --nogui
+#bundle exec veewee vbox validate 'ubuntu-14'
+#bundle exec vagrant basebox export 'ubuntu-14' --force
