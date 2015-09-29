e3cb88bf9011c2fc5182ff1986b3ca7c26d9b698
saanindia <saan.india@gmail.com>
2015-09-29 08:33:29 +0530

    Update auto_install.sh

--- auto_install.sh
+++ auto_install.sh
@@ -2,13 +2,17 @@
 
 cd /home/AutoVM
 vagrant up
-vagrant ssh
+vagrant ssh -c "sudo service puppet status"
+vagrant ssh -c "sudo puppet agent --test --waitforcert=60"
 
-echo "Transfered successfully"
-touch yes_test.sh
+
+sudo puppet cert sign --all
+vagrant ssh -c "sudo puppet agent --test"
+sudo exit
+
+echo "Work successfully Done"
 
 #bundle install
-
 #bundle exec veewee vbox define 'ubuntu-14' 'ubuntu-14.04.3-server-amd64'
 #bundle exec veewee vbox build 'ubuntu-14' --force --auto --nogui
 #bundle exec veewee vbox validate 'ubuntu-14'
